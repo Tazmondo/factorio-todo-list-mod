211d585900eea6a233b1edd00e06844bf55f6c84
Tazmondo <fondcoolman@outlook.com>
2021-02-01 00:09:07 +0000

    finished initial version. can now create and remove tasks from task list

--- control.lua
+++ control.lua
@@ -1,45 +1,126 @@
-local mod_gui = require('mod-gui')
+mod_gui = require('mod-gui')
 
-local x = 1
-local y
 
-local myfunction = function(event)
-    game.print(string.rep(math.random(1000), 100), {math.random(), math.random(), math.random(), math.random()})
-    x = x + 1
-end
+function guistuff(player)
+    local ptable = global.players[player.index]
 
-local guistuff = function(player)
-    if not global.guiloaded[player.name] then
+    if not global.guiloaded[player.index] then
 
         local modbuttons = mod_gui.get_button_flow(player)
+        local modguis = mod_gui.get_frame_flow(player)
 
-        if modbuttons.todo_list then modbuttons.todo_list.destroy() end
-        y = modbuttons.add({type="sprite-button", name="todo_list", sprite="todo-list-top-button-sprite"})
+        if modbuttons.todo_list_top_button then modbuttons.todo_list_top_button.destroy() end
 
-        global.guiloaded[player.name] = true
+        ptable.topbutton =
+        modbuttons.add({type="sprite-button", name="todo_list_top_button", sprite="todo-list-top-button-sprite"})
+
+        if modguis.todo_list_main_frame then modguis.todo_list_main_frame.destroy() end
+
+        ptable.maingui =
+        modguis.add({
+            type = "frame",
+            name = "todo_list_main_frame",
+            caption = "Todo List",
+            direction = "vertical",
+            visible = false
+        })
+
+        ptable.task_list = ptable.maingui.add({
+            type = "scroll-pane",
+            vertical_scroll_policy = "auto-and-reserve-space",
+            horizontal_scroll_policy = "never"
+        })
+        ptable.task_list.style.height = 400
+        ptable.task_list.style.width = 200
+
+        local a = ptable.maingui.add({
+            type = "flow",
+        })
+
+        ptable.new_task =
+        a.add({
+            type = "textfield"
+        })
+
+        a.add({
+            type = "sprite",
+            sprite = "utility/add"
+        })
+
+
+        global.guiloaded[player.index] = true
     end
+end
+
+function makenewtask(player, task)
+    local ptable = global.players[player.index]
+
+    local newflow = ptable.task_list.add({
+        type = "flow",
+        direction = "horizontal"
+    })
+    newflow.style.padding = {5,0,5,0}
+    newflow.style.width = 200
+
+    local newlabel = newflow.add({
+        type = "label",
+        caption = task
+    })
+    newlabel.style.single_line = false
+    newlabel.style.width = 155
+
+    local newcompletebutton = newflow.add({
+        type = "sprite-button",
+        sprite = "utility/check_mark_green",
+        name = "todo_list_task_complete_button"
+    })
+    newcompletebutton.style.horizontal_align = "right"
+    newcompletebutton.style.vertical_align = "bottom"
+    newcompletebutton.style.width = 35
 end
 
 script.on_init(function()
     global.guiloaded = {}
+    global.players = {}
 
     for _, player in pairs(game.players) do
-        global.guiloaded[player.name] = false
+        global.guiloaded[player.index] = false
+        global.players[player.index] = {}
+
         guistuff(player)
     end
 end)
 
-script.on_event({defines.events.on_player_created}, function(event)
+script.on_load(function()
+    log(serpent.block(global))
+end)
+
+script.on_event({defines.events.on_player_created, defines.events.on_console_chat}, function(event)
     local player = game.players[event.player_index]
-    global.guiloaded[player.name] = false
+
+    global.guiloaded[player.index] = false
+    global.players[player.index] = {}
+
     guistuff(player)
 end)
 
-script.on_event(defines.events.on_gui_click, function(event)
+script.on_event({defines.events.on_gui_click, defines.events.on_gui_confirmed}, function(event)
     local element = event.element
     local player = game.players[event.player_index]
+    local ptable = global.players[player.index]
 
-    if element == y then
-        myfunction(nil)
+    if element.name == "todo_list_top_button" then
+        if ptable.maingui then
+            ptable.maingui.visible = not ptable.maingui.visible
+            ptable.new_task.text = ""
+        end
+    elseif element == ptable.new_task then
+        local tasktitle = ptable.new_task.text
+        ptable.new_task.text = ""
+        if #tasktitle > 0 then
+            makenewtask(player, tasktitle)
+        end
+    elseif element.name == "todo_list_task_complete_button" then
+        element.parent.destroy()
     end
 end)
